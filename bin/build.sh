b61a702258cf9aa886449d51cb28f3e2f2ad57d3
Perry Bhandal <perry.bhandal@gmail.com>
2014-08-30 05:20:32 -0400

    Added build script.

--- bin/build.sh
+++ bin/build.sh
@@ -1,1 +1,31 @@
+SRC_DIR="../src/"
+BUILD_DIR="../build/"
+# Show all warnings.
+COMPILER_WARNINGS_FLAG="-Wall"
 
+# Flag to ensure we can create position independent code (needed for creating lib objects)
+GLOBAL_FLAGS="-fPIC"
+
+function build()
+{
+    cmake $SRC_DIR \
+        -G "Unix Makefiles" \
+        -DCMAKE_CXX_FLAGS=$GLOBAL_FLAGS \
+        -DCMAKE_C_FLAGS=$GLOBAL_FLAGS \
+        -DCMAKE_EXE_LINKER_FLAGS=$GLOBAL_FLAGS \
+        -DCMAKE_MODULE_LINKER_FLAGS=$GLOBAL_FLAGS \
+        -DCMAKE_SHARED_LINKER_FLAGS=$GLOBAL_FLAGS \
+        -DCMAKE_CXX_FLAGS=$COMPILER_WARNINGS_FLAG \
+            || { echo "Compilation failed."; exit 1; }
+}
+
+if [ ! -d $BUILD_DIR ]; then
+    mkdir $BUILD_DIR
+fi
+
+pushd $BUILD_DIR > /dev/null
+
+build
+make
+
+popd > /dev/null
